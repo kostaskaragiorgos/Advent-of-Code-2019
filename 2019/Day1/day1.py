084c32479624c0ec01b083d593fb8e8ddc800342
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-04-13 13:54:20 +0300

    Update day1.py

--- 2019/Day1/day1.py
+++ 2019/Day1/day1.py
@@ -4,11 +4,18 @@ import math
 def fuel_requirements(x):
     return (math.floor(x / 3) - 2)
 
-
+#part 1 tests
 assert fuel_requirements(12) ==2
 assert fuel_requirements(14) == 2
 assert fuel_requirements(1969) == 654
 assert fuel_requirements(100756)==33583
+
+
+
+#part2 tests
+# assert 14 == 2
+# assert 1969 == 966
+# assert 100756 == 50346 
 
 sum = 0
 with open("input.txt", "r") as file:
