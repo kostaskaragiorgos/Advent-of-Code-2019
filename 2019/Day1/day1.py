a71cd9353abbc085d4c3d3c4d003a158ffee39a2
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-04-15 09:48:15 +0300

    Update day1.py

--- 2019/Day1/day1.py
+++ 2019/Day1/day1.py
@@ -1,8 +1,9 @@
-import math
-
 # part 1 
 def fuel_requirements(x):
-    return (math.floor(x / 3) - 2)
+    return (x // 3 - 2)
+
+def mass(x):
+    return x
 
 #part 1 tests
 assert fuel_requirements(12) ==2
@@ -12,14 +13,25 @@ assert fuel_requirements(100756)==33583
 
 
 
-#part2 tests
-# assert 14 == 2
-# assert 1969 == 966
-# assert 100756 == 50346 
+#part2
 
-sum = 0
+def fuel_for_fuel(x):
+    sum = 0
+    while fuel_requirements(x) >= 0:
+        x = fuel_requirements(x)
+        sum += x
+    return sum
+#part2 tests
+
+assert fuel_for_fuel(14) == 2
+assert fuel_for_fuel(1969) == 966
+assert fuel_for_fuel(100756) == 50346 
+
+p1sum = 0
+p2sum = 0
 with open("input.txt", "r") as file:
     for line in file:
         mass = line.strip()
-        sum += fuel_requirements(int(mass))
-print(sum)
+        p1sum += fuel_requirements(int(mass))
+        p2sum += fuel_for_fuel(int(mass))
+print(p2sum)
